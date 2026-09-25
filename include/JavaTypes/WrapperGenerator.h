5096d73ab8ae47b875647b1ed9d57f0c1effa3a9
Addo6544 <addosama@icloud.com>
2026-09-25 18:42:34 +0800

    idk

--- include/JavaTypes/WrapperGenerator.h
+++ include/JavaTypes/WrapperGenerator.h
@@ -14,6 +14,17 @@
 #define FROSTJNI_JAVATYPES_WRAPPER_IMPL(Name, Super) class FROSTJNI_JAVATYPES_WRAPPER(Name) : public FROSTJNI_JAVATYPES_WRAPPER(Super)
 #define FROSTJNI_JAVATYPES_WRAPPER_FWD(Name) class FROSTJNI_JAVATYPES_WRAPPER(Name);
 
+#define FROSTJNI_JAVATYPES_MethodWrapperBody_
+#define FROSTJNI_JAVATYPES_MethodWrapperBody_Decl ;
+#define FROSTJNI_JAVATYPES_MethodWrapperBody_BasicCall(Class, Name, Desc, Modifier, Type)\
+    {  static const jmethodID id = FROSTJNI_NAMESPACE_ Context->Get##Modifier##MethodID(\
+        FROSTJNI_NAMESPACE_ Context->FindClass(Class, nullptr), Name, Desc);\
+        return FrostJNI::Context->GetThreadEnv()->Call##Modifier##Type##Method(get(), id); }
+
+#define FROSTJNI_JAVATYPES_ObjectMethodWrapper(Body, RetType, Name, ...)\
+    FROSTJNI_JAVATYPES_WRAPPER(RetType) Name(__VA_ARGS__) const noexcept FROSTJNI_JAVATYPES_MethodWrapperBody_##Body
+#define FROSTJNI_JAVATYPES_BasicMethodWrapper(Body, RetType, Name, ...)\
+    RetType Name(__VA_ARGS__) const noexcept FROSTJNI_JAVATYPES_MethodWrapperBody_##Body
 
 #endif // FROSTJNI_WRAPPER_GENERATORS_DEFINED
 
